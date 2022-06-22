63110470b7f093bbdf1af5bcf1d2d090c2ed6fc4
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-06-22 14:18:34 +0530

    First Commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "React.js is a free and open-source front-end ...",
+      optionA: "JavaScript library",
+      optionB: "Bootstrap library",
+      optionC: "CSS library",
+      optionD: "None of the Above",
+      answer: "optionA",
+    },
+    {
+      prompt: "React.js had Written in ...",
+      optionA: "Php",
+      optionB: "Python",
+      optionC: "Java",
+      optionD: "JavaScript",
+      answer: "optionD",
+    },
+    {
+      prompt: "Which of the following command is used to create a react app?",
+      optionA: "install -g create-react-app",
+      optionB: "npm install create-react-app",
+      optionC: "npx create-react-app reactapp",
+      optionD: "None of the Above",
+      answer: "optionB",
+    },
+  ];
